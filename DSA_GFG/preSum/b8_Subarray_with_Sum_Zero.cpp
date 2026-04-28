bc455ad9552ab71bba36232a4a095ed9e055f587
chanhisme <chanhle1907@gmail.com>
2026-04-28 10:59:57 +0700

    code Subarray with Sum Zero with prefix sum

--- DSA_GFG/preSum/b8_Subarray_with_Sum_Zero.cpp
+++ DSA_GFG/preSum/b8_Subarray_with_Sum_Zero.cpp
@@ -1,35 +1,32 @@
 #include <iostream>
-#include <vector>
 #include <unordered_map>
-using  namespace std;
-#define ll long long
+#include <vector>
 
+using namespace std;
+
+#define ll long long
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+    //maincode
     int n;
     cin >> n;
-    vector <int> a(n);
-    unordered_map <ll, int> cnt;
-    for(int &x : a){
-        cin >> x;
-        if(x == 0){
-            cout<<"true";
-            return 0;
-        }
+    vector <int> a(n), pf(n);
+    for(int &x : a) cin >> x;
+    unordered_map <int, int> cnt;
+    pf[0] = a[0];
+    for(int i = 1; i < n; i++){
+        pf[i] = pf[i-1] + a[i];
     }
-    ll sum = 0;
-    cnt[0] = -1;
     int ok = 0;
     for(int i = 0; i < n; i++){
-        sum += a[i];
-        if(cnt.count(sum)){
+        if(cnt.count(pf[i]) ){
             ok = 1;
+            break;
         }
         else{
-            cnt[sum] = i;
+            cnt[pf[i]] = i;
         }
     }
     if(ok) cout<<"true";
@@ -43,3 +40,5 @@ int main(){
 
     return 0;
 }
+
+
