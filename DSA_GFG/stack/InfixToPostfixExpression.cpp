bc381e5f9704cbfc93d0c15bc4f106e99b43a64d
chanhisme <chanhle1907@gmail.com>
2026-05-31 20:17:50 +0700

    stack problems...

---
+++ DSA_GFG/stack/InfixToPostfixExpression.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stack>
+#include <ctype.h>
+using namespace std;
+
+bool validVar(char ch){
+    if(isalpha(ch) || isdigit(ch)){
+        return true;
+    }
+    return false;
+}
+bool validOp(char ch){
+    if(ch == '+' || ch == '-' || ch == '/' || ch == '*' || ch == '^'){
+        return true;
+    }
+    return false;
+    
+}
+int prio(char ch){
+    if(ch == '^') return 3;
+    else if(ch == '*' || ch == '/') return 2;
+    else if(ch == '+' || ch == '-') return 1;
+    return -1;
+}
+
+int main(){
+    string res = "";
+    stack <char> op;
+    string s;
+    cin >> s;
+    for(int i = 0; i < s.size(); i++){
+        if(validVar(s[i])){
+            res += s[i];
+        }
+        else if(s[i] == '('){
+            op.push('(');
+        }
+        else if(s[i] == ')'){
+            while(!op.empty() && op.top() != '('){
+                res += op.top();
+                op.pop();
+            }
+            if(!op.empty()){
+                op.pop();
+            }
+        }
+        else{
+            while(!op.empty() && op.top() != '(' && prio(op.top()) >= prio(s[i])){
+                res += op.top();
+                op.pop();
+            }
+            op.push(s[i]);
+        }
+    }
+    while(!op.empty()){
+        res += op.top();
+        op.pop();
+    }
+    cout<<res;
+    
+
+
+
+
+
+    return 0;
+}
---
+++ DSA_GFG/stack/intro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+    stack <char> st;
+    
+    
+
+
+
+
+
+    return 0;
+}
