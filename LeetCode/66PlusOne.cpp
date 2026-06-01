c6cb1074971a49ead58c3647dcb91c5461f226ae
chanhisme <chanhle1907@gmail.com>
2026-06-01 09:38:08 +0700

    stack problems

---
+++ LeetCode/66PlusOne.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int myInt(char c){
+    return c - '0';
+}
+int main(){
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for(int &x : a ) cin >> x;
+    for(int i = 0; i< n; i++){
+        
+    }
+
+    return 0;
+}
