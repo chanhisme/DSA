2dc49a5790cd70a87285bc625b8bb34bd1f5b5d7
chanhisme <chanhle1907@gmail.com>
2026-06-04 20:07:44 +0700

    Add infix/postfix converters; fix Plus One
    Add stack-based expression conversion implementations: DSA_GFG/stack/InfixToPrefixNotation.cpp and PostfixToInfix.cpp, along with local .cph problem metadata for their tests. Fix LeetCode/66PlusOne.cpp to propagate carries correctly and prepend 1 when all digits roll over; update the .cph test expectation accordingly. Also add a skeleton for LeetCode/67AddBinary.cpp and include a new LeetCode/osg.exe binary.

--- LeetCode/66PlusOne.cpp
+++ LeetCode/66PlusOne.cpp
@@ -11,9 +11,19 @@ int main(){
     cin >> n;
     vector <int> a(n);
     for(int &x : a ) cin >> x;
-    for(int i = 0; i< n; i++){
-        
+    for(int i = n-1; i >= 0; i--){
+        if(a[i] < 9){
+            a[i]++;
+            break;
+        }
+        else{
+            a[i] = 0;
+        }
     }
+    if(a[0] == 0){
+        a.insert(a.begin(), 1);
+    }
+    for(int x : a) cout<<x;
 
     return 0;
 }
