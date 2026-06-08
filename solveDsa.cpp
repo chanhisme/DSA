6dab73a21c2764a3fe95c4245d1b0028d5f91e8a
chanhisme <chanhle1907@gmail.com>
2026-06-08 09:52:20 +0700

    Use two-pointer to count subarrays in solveDsa.cpp
    Replace the previous unordered_map pair-counting approach with a sliding-window (two-pointer) algorithm that counts subarrays with sum >= k (computes total subarrays minus those with sum < k). Change result accumulator to long long to avoid overflow for large n. Add new local test cases (.cph file updated) and include a new binary randomProblems.exe. Commented-out experimental code left in place.

--- solveDsa.cpp
+++ solveDsa.cpp
@@ -3,27 +3,39 @@
 #include <unordered_map>
 using namespace std;
 
-
 int main(){
     unordered_map <int, int> cnt;
-    int res = 0;
+    long long res = 0;
     int n, k;
     cin >> n >> k;
     vector <int> a(n);
-    for(int &x : a ) cin >> x;
-    for(int i = 0; i < n; i++){
-        int x = a[i];
-        if(k == 0){
-            res += cnt[x];
+    for(int &x : a) cin >> x;
+    long long s = 0;
+    int r = 0;
+    int l = 0;
+    
+    // for(int l = 0; l < n; l++){
+    //     while(r < n && s < k){
+    //         s += a[r];
+    //         r++;
+    //     }
+    //     if(s >= k){
+    //         res += (n-r+1);
+    //     }
+    //     s -= a[l];
+    // }
+    // cout<<res;
+
+
+    for(; r < n; r++){
+        s += a[r];
+        while(s >= k){
+            s -= a[l];
+            l++;
         }
-        else{
-            res += cnt[x-k];
-            res += cnt[x + k];
-        }
-        
-        cnt[x]++;
+        res += (r-l + 1);
     }
-    cout<<res;
+    cout<< ( n * (n+1))/2 - res; 
 
 
     return 0;
