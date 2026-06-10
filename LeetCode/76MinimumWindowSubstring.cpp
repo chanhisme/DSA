73fd97b59e1db0c70d8b78ad6dea65b213400d0c
chanhisme <chanhle1907@gmail.com>
2026-06-10 08:03:04 +0700

    optimize by using couunting sort

--- LeetCode/76MinimumWindowSubstring.cpp
+++ LeetCode/76MinimumWindowSubstring.cpp
@@ -4,44 +4,50 @@
 using namespace std;
 
 int main(){
-    unordered_map <char, int> cnt;
+    int cnt[128] = {0};
+    int cnt_size = 0;
     string s, t;
     cin >> s >> t;
     int n = s.size();
     for(int i = 0; i < t.size(); i++){
+        if(cnt[t[i]] == 0){
+            cnt_size ++;
+        }
         cnt[t[i]]++;
+        
     }
     int l = 0;
     int curr = 0;
     int len = n+1;
-    unordered_map <char, int> ok;
+    int st;
+    int ok[128] = {0};
         string res;
         for(int r= 0 ; r < n; r++){
-            if(cnt.count(s[r])){
+            if(cnt[s[r]]){
                 ok[s[r]]++;
                 if(ok[s[r]] == cnt[s[r]]){
-                curr++;
+                    curr++;
             }
             
         }
         
-        while(cnt.size() == curr){
+        while(cnt_size == curr){
             if( (r-l+1) < len){
                 len = (r-l+1);
-                res = s.substr(l, r-l+1);
+                st = l;
             }
-            if(cnt.count(s[l])){
+            if(cnt[s[l]]){
                  ok[s[l]] --;
                  if(ok[s[l]] < cnt[s[l]]){
                     curr--;
-                    ok.erase(s[l]);
                 }
             }
             
             l++;
         }
     }
-    cout<<res;
+    if(len == n-1) cout<<"";
+    else cout<<s.substr(st, len);
 
 
 
