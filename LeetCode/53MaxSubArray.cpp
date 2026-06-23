4fd77bc098763b62a25fa79e3ca6c58b2042774d
chanhisme07 <chanhle1907@gmail.com>
2026-06-23 13:14:38 +0700

    update solve edge case

--- LeetCode/53MaxSubArray.cpp
+++ LeetCode/53MaxSubArray.cpp
@@ -8,7 +8,7 @@ int main(){
     int n; cin >> n;
     vector <int> a(n);
     for(int &x : a) cin >> x;
-    long long best_sum = 0;
+    long long best_sum = a[0];
     long long curr = 0;
     //kadane algorithm
     for(int i = 0; i < n; i++){
