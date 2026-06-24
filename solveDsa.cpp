d177a49c059c2a4f596f4665109f4507f1687e34
chanhisme <chanhle1907@gmail.com>
2026-06-24 13:26:22 +0700

    test

--- solveDsa.cpp
+++ solveDsa.cpp
@@ -8,22 +8,6 @@ using namespace std;
 
 int main(){
 
-    int n, k; cin >> n >> k;
-    vector <int> a(n);
-    for(int &x : a) cin >> x;
-
-    int l = 0;
-    int curr_sum = 0, res = 0;
-    for(int r = 0; r < n; r++){
-        curr_sum += a[r];
-        while(l <= r && curr_sum > k){
-            curr_sum -= a[l];
-            l++;
-        }
-        res = max(res, curr_sum);
-    }
-    cout<<res;
-
 
 
     return 0;
