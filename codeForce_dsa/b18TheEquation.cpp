94ccfbbaf1bc6f1874a555172bc11b7f5fb16f43
chanhisme <chanhle1907@gmail.com>
2026-07-04 13:36:29 +0700

    the equation problem CF

--- codeForce_dsa/b18TheEquation.cpp
+++ codeForce_dsa/b18TheEquation.cpp
@@ -1,50 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int ext_euclid(int a, int b, int &x, int &y){
-    if(b == 0){
+int ext_euclid(int a, int b, int &x, int &y) {
+    if (b == 0) {
         x = 1;
         y = 0;
         return a;
     }
-    
-    int q = a/b;
-    int r = a%b;
-    int x1 =0, y1 = 0;
-    int d = ext_euclid(b, r, x1, y1);
+    int x1, y1;
+    int d = ext_euclid(b, a % b, x1, y1);
     x = y1;
-    y = x1 - q * y1;
+    y = x1 - (a / b) * y1;
     return d;
 }
-pair <int, int> sol = {INT_MAX, INT_MAX};
 
-pair <int, int> diophantineSolve(int a, int b, int c){
-    int x = 0, y = 0;
-    int d = ext_euclid (a, b, x, y);
-    if(c/d != 0){
-        return sol;
-    }
-    x *= (c/d);
-    y *= (c/d);
-    if(a < 0) x = -x;
-    if(b < 0) y = -y;
-    return make_pair(x,y);
-}
-int cnt(pair <int, int> res, int x1, int x2, int y1, int y2){
-    int cnt_x = floor(x2/res.first) - floor(x1/res.first) + 1;
-    int cnt_y = floor(y2/res.second) - floor(y1/res.second) + 1;
-    return min(cnt_x, cnt_y); 
-}
-
-
-
-
-int main(){
-    int a,b,c;
-    cin >> a >> b >> c;
-    int x1,x2,y1,y2;
-    cin >> x1 >> x2;
-    cin >> y1 >> y2;
+pair<int, int> diophantineSolve(int a, int b, int c) {
+    c = -c;
     int x, y;
-    cout<<cnt(diophantineSolve(a,b,c), x1, x2, y1, y2);
+    int d = ext_euclid(a, b, x, y);
+    if (c % d != 0) {
+        return {INT_MAX, INT_MAX};
+    }
+    int scale = c / d;
+    x *= scale;
+    y *= scale;
+    return {x, y};
+}
+
+int cnt(pair<int, int> res, int x1, int x2, int y1, int y2, int a, int b) {
+    int x0 = res.first, y0 = res.second;
+    if (x0 == INT_MAX) return 0;
+
+    int d = gcd(abs(a), abs(b));
+    int stepX = b / d;
+    int stepY = a / d;
+
+    int kx1 = (int)ceil((double)(x1 - x0) / stepX);
+    int kx2 = (int)floor((double)(x2 - x0) / stepX);
+    int ky1 = (int)ceil((double)(y0 - y2) / stepY); // 🔥 SỬA LẠI CHỖ NÀY
+    int ky2 = (int)floor((double)(y0 - y1) / stepY); // 🔥 SỬA LẠI CHỖ NÀY
+
+    int k_min = max(kx1, ky1);
+    int k_max = min(kx2, ky2);
+
+    if (k_min > k_max) return 0;
+    return k_max - k_min + 1;
+}
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int x1, x2, y1, y2;
+    cin >> x1 >> x2 >> y1 >> y2;
+
+    pair<int, int> res = diophantineSolve(a, b, c);
+    cout << cnt(res, x1, x2, y1, y2, a, b) << '\n';
+    return 0;
 }
