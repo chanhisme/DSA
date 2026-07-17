692197fb5a7b3843c57ec2601292e455dbd37d87
chanhisme <chanhle1907@gmail.com>
2026-07-18 00:14:44 +0700

    update

--- vnoi/stackk/b17ImplementQueueUsingStacks.cpp
+++ vnoi/stackk/b17ImplementQueueUsingStacks.cpp
@@ -1,22 +1,54 @@
-#include <iostream>
-#include <stack>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 struct Q{
-    stack <int> a;
-
+    stack <int> a, b;
+    int my_size = 0;
+    void move(){
+        if(b.empty()){
+            while(!a.empty()){
+                b.push(a.top());
+                a.pop();
+            }
+        }
+    }
     void enqueue(int x){
         a.push(x);
+        my_size ++;
     }
     void dequeue(){
-        
+        move();
+        b.pop();
+        my_size--;
+    }
+    int front(){
+        move();
+        return b.top();
+    }
+    int size(){
+        return my_size;
+    }
+    bool empty() {
+        return my_size == 0;
     }
 };
-
+#define nl '\n'
 int main(){
-
-
-
+    
+    Q q;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        q.enqueue(x);
+    }
+    
+    cout<<q.size()<<nl;
+    cout<<q.front()<<nl;
+    q.dequeue();
+    cout<<q.front()<<nl;
+    
 
 }
